2f0c7b6346095c0c722670330f6064588330d9a4
Mayuri Patkar <72700753+mayuripatkar@users.noreply.github.com>
2020-11-05 21:10:37 +0530

    Add files via upload

---
+++ Emailslicer.py
@@ -0,0 +1,4 @@
+email = input("Enter your email-id:").strip()
+user_name = email[:email.index("@")]
+domain_name = email[email.index("@")+1:]
+print("Your username is '{}' while domain name is '{}'".format(user_name,domain_name))
