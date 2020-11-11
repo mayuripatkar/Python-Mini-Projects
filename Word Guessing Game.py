5506ef75ca07526058712b3c1464a15fa0687411
Mayuri Patkar <mayurigpatkar@gmail.com>
2020-11-11 16:03:59 +0530

    Python mini project added

---
+++ Word Guessing Game.py
@@ -0,0 +1,29 @@
+import random
+name = input("What's your name:")
+
+companies = ['Facebook','Apple','Netflix','Google','Amazon','Microsoft','Adobe']
+company = random.choice(companies)
+
+print("Hey",name,"guess a tech company!")
+guesses = ''
+attempts = 10
+
+while attempts > 0:
+    fail = 0
+    for char in company:
+        if char in guesses:
+            print(char)
+        else:
+            print("_")
+            fail += 1
+    if fail == 0:
+        print("You won.The word is:",company)
+        break
+    guess = input("Guess a tech company:")
+    guesses += guess
+    if guess not in company:
+        attempts -= 1
+        print("Wrong")
+        print("You have",attempts,"more guesses")
+    if attempts == 0:
+        print("You lost")
