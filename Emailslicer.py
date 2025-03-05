5d19880aa1f1c199d35f9a189d36d9de5f77b165
Mayuri Patkar <72700753+mayuripatkar@users.noreply.github.com>
2025-03-05 22:11:03 +0530

    Update Emailslicer.py

--- Emailslicer.py
+++ Emailslicer.py
@@ -1,4 +1,4 @@
-email = input("Enter your personal email-id:").strip()
+email = input("Enter your personal or official email-id:").strip()
 user_name = email[:email.index("@")]
 domain_name = email[email.index("@")+1:]
 print("Your username is '{}' while domain name is '{}'".format(user_name,domain_name))
